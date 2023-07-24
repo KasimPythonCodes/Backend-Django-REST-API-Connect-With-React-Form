945ccfc49e3b5dcd7a901cc4853f3888f73f5edd
kasim saifi <kasimsaifi9990@gmail.com>
2023-07-24 13:04:29 +0530

    changed

--- connectwithreact/settings.py
+++ connectwithreact/settings.py
@@ -121,7 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR   , "media")
+
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS =[
+    (
+        os.path.join(BASE_DIR  , 'static')
+    )
+    ]
 
 # CORS_ALLOWED_ORIGINS = [
 # "http://127.0.0.1:8000",
